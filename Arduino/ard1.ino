63817c252962e34f39e0ecb79c3cbfd00e403144
Sammy-Barasa <kesammybarasa@gmail.com>
2021-07-22 12:53:07 +0300

    initial setup

---
+++ Arduino/ard1.ino
@@ -0,0 +1,35 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  // sensor 1
+
+  // sensor 2
+
+  // LED
+
+  // LED State
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // setting up detecting direction
+
+    // 
+
+  // setting up wi-fi module
+
+
+    // streaming to our API
+
+
+        // LED state
+        
+}
+
+//  **************** FUNCTIONS ***************
+
+
+
+// debouncing function
