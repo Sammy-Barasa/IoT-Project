dc4fde630f0d7c7d9ddda891cc83f6920ca14cf6
Sammy-Barasa <kesammybarasa@gmail.com>
2021-07-26 10:20:18 +0300

    added room model

--- Backend/DeviceManager/models.py
+++ Backend/DeviceManager/models.py
@@ -2,17 +2,28 @@ from django.db import models
 
 # Create your models here.
 
+
+class Room(models.Model):
+    number = models.IntegerField()
+    occupied = models.BooleanField()
+    lastActivity = models.DateField(auto_now=True)
+    
+
+    def __str__(self):
+        return self.number
+
 class Device(models.Model):
     CHOICES=[
         ('OCCUPANT','OCCUPANT'),
         ('ADMIN','ADMIN')
     ]
+    deviceIDRef = models.CharField(max_length=20,default="01XYZ")
     ledState = models.BooleanField()
-    roomNumber=models.IntegerField()
+    roomNumber=models.ForeignKey("Room",on_delete=models.CASCADE)
     lastToggle = models.DateField( auto_now=True)
     lastUser = models.CharField(max_length=10,choices=CHOICES)
     
 
     def __str__(self):
-        return self.roomNumber
+        return self.deviceIDRef
    
